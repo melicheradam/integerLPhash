b794117609cf967736711859963bdbb7831ab753
melicheradam <addik36@gmail.com>
2020-04-16 14:26:25 +0200

    uprava main

--- LinearProbingHash.c
+++ LinearProbingHash.c
@@ -137,118 +137,21 @@ void linear_probing_print(LinearProbing* table) {
     printf("[table end]\n");
 }
 
-//////////////////////////////////////////////////////////////////////
-//FUNGOVANIE TESTOVACEJ FUNKCIE JE NAJLEPSIE OPISANE V HASHING_CHAIN//
-//////////////////V OSTATNYCH LEN PROMPTNE////////////////////////////
-//////////////////////////////////////////////////////////////////////
 
-int runLpHash(pocetPrvkov, pocetHladani) {
-
-    //ak prekrocim load factor 0,7 tak zvacsim tabulku o pocetPrvkov + 30%
-
-    printf("\n-------PREVZATY LP HASH-------\n");
-    int dalsiPP, dalsiPS, oldPocet, kolizie = 0, i = 0;
-    double loadF;
-    time_t pociatocny, koncovy;
-
-    //nastavim velkost na 0,7 load factor
-    int size = (pocetPrvkov * 130) / 100;
-
-    LinearProbing* table = linear_probing_init(size);
-    
-    //vkladanie do HASH pola
-    pociatocny = clock();
-    for (i = 0; i < pocetPrvkov; i++) {
-        if (linear_probing_set(table, poleCisel[i]) == 1)kolizie++;
-    }
-    koncovy = clock();
-
-    printf("Test vkladania %d prvkov trval: %.2lfs\n", pocetPrvkov, (double)(koncovy - pociatocny) / CLOCKS_PER_SEC);
-    
-    //logovanie casu
-    ulozCas((double)(koncovy - pociatocny) / CLOCKS_PER_SEC);
-
-    //zamiesanie pola cisel
-    shuffle(pocetPrvkov, pocetPrvkov);
-
-    //hladanie v HASH poli
-    pociatocny = clock();
-    for (i = 0; i < pocetHladani; i++) {
-        if (linear_probing_get(table, poleCisel[i % pocetPrvkov]) == -1) return i;
-    }
-    koncovy = clock();
-
-    printf("Test hladania %d prvkov trval: %.2lfs\n", pocetHladani, (double)(koncovy - pociatocny) / CLOCKS_PER_SEC);
-    printf("---%.2lf%% KOLIZII---\n", (double)((double)kolizie / (double)pocetPrvkov) * 100);
-    
-    //logovanie casu
-    ulozCas((double)(koncovy - pociatocny) / CLOCKS_PER_SEC);
-
-    printf("\nZadaj 0 pre ukoncenie\nVkladat a vyhladavat sa budu nahodne prvky od 1-10m\n");
-    do {
-        printf("----------------------\n");
-        printf("Zadaj pocet prvkov na vlozenie: ");
-        scanf("%d", &dalsiPP);
-        printf("Zadaj pocet prvkov na hladanie: ");
-        scanf("%d", &dalsiPS);
-        printf("----------------------\n");
-        if (!dalsiPP && !dalsiPS)break;
-
-        //ak nahodou prekrocim hranicu 30% volnych prvkov zvacsujem tabulku
-        oldPocet = pocetPrvkov;
-        pocetPrvkov += dalsiPP;
-        loadF = pocetPrvkov / table->size;
-        if (loadF > 0.7)table = linear_probing_resize(table, (pocetPrvkov * 130) / 100);
-
-        //dodatocne vkladanie
-        pociatocny = clock();
-        for (i = 0; i < dalsiPP; i++) {
-            if (linear_probing_set(table, getRand(MOJ_RND_MAX)) == 2)pocetPrvkov--;
-        }
-        koncovy = clock();
-
-        printf("Test vkladania %d prvkov do %d trval: %.2lfs\n", dalsiPP, oldPocet, (double)(koncovy - pociatocny) / CLOCKS_PER_SEC);
-        
-        //logovanie casu
-        ulozCas((double)(koncovy - pociatocny) / CLOCKS_PER_SEC);
-        
-        //hladanie v HASH poli
-        pociatocny = clock();
-        for (i = 0; i < dalsiPS; i++) {
-            linear_probing_get(table, getRand(MOJ_RND_MAX));
-        }
-        koncovy = clock();
-
-        printf("Test hladania %d prvkov v %d trval: %.2lfs\n", dalsiPS, pocetPrvkov, (double)(koncovy - pociatocny) / CLOCKS_PER_SEC);
-        
-        ulozCas((double)(koncovy - pociatocny) / CLOCKS_PER_SEC);
-    
-    } while (dalsiPP != 0 && dalsiPS != 0);
-
-    linear_probing_free(table);
-    return 0;
-}
-
-
-
-/* zakomentovane kvoli testovacu
 int main() {
-    FILE* subor;
-    int i;
-    i = 0;
-    int pocetPrvkov, pocetHladani;
+    LinearProbing *tabulka;
+    //inicializacia tabulky
+    tabulka = linear_pribing_init(500);
+    //vkladanie
+    linear_probing_set(tabulka, 10);
+    //hladanie
+    linear_probing_get(tabulka, 10);
+    //resize tabulky
+    tabulka = linear_probing_resize(tabulka,1000);
 
-    pocetPrvkov = 1000;
-    pocetHladani = 5000;
-
-    poleCisel = malloc(pocetPrvkov * sizeof(int));
-    subor = fopen("dataset1.txt", "r");
-    while (fscanf(subor, " %d", (poleCisel + i)) != EOF && i < pocetPrvkov)i++;
-    fclose(subor);
-
-    runLpHash(pocetPrvkov, pocetHladani);
+   
     return 0;
 }
-*/
+
 
 
